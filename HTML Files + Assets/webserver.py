0c4be99ee9579d6b550b7fff03aac2d8451645f3
JamilVelez <60303573+JamilVelez@users.noreply.github.com>
2024-02-24 21:50:20 -0500

    HTML Files from GCM Bootstrap Design
    Exported the HTML files along with their assets to a new folder. Added the Google Translate button (horizontal) to the Homeapage.html.
    Made a basic Webserver.py as I was running into issues with browsers trying to open the HTML file and running the Google Translate API. The scriptLang.js file is currently unused but will try to implement it as it's for the Google Translate bar to not appear after translation.

---
+++ HTML Files + Assets/webserver.py
@@ -0,0 +1,17 @@
+# https://reintech.io/blog/how-to-create-a-simple-web-server-with-python
+import http.server
+import socketserver
+
+# Define the handler to use for incoming requests
+handler = http.server.SimpleHTTPRequestHandler
+
+# Set the port number for the server
+port = 8080
+
+# Create the server and bind it to the specified port
+httpd = socketserver.TCPServer(("", port), handler)
+
+print(f"Serving on port {port}")
+
+# Run the server indefinitely
+httpd.serve_forever()
