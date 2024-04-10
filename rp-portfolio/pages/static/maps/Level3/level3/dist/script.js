a632bac76624245a1d91b0033310dba1d4bde662
annierome1 <112560966+annierome1@users.noreply.github.com>
2024-04-10 10:05:42 -0400

    Map fixes and file clean up

--- rp-portfolio/pages/static/maps/Level3/level3/dist/script.js
+++ rp-portfolio/pages/static/maps/Level3/level3/dist/script.js
@@ -4,10 +4,4 @@ $(document).ready(function(){
      $(".hide").hide();
     $(".BigJohn-txt").fadeIn(300);
   });
-
-  $(".LearningCenter").click(function(){
-     $(".hide").hide();
-    $(".LearningCenter-txt").fadeIn(300);
-  });
-  
 });
