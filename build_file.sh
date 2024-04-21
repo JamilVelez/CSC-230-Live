1122745038837e43731e51b96218e8bcdff83baf
JamilVelez <60303573+JamilVelez@users.noreply.github.com>
2024-04-21 19:58:34 -0400

    Update build_file.sh

--- build_file.sh
+++ build_file.sh
@@ -1,6 +1,4 @@
-python -m ensurepip
-python -m pip install --upgrade pip
 
 # Install requirements
-pip install -r requirements.txt
+python3 install -r requirements.txt
 python3.9 manage.py collectstatic
