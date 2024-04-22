749e9d2d6b988d71170d4eb41d71e88bae1bc289
JamilVelez <60303573+JamilVelez@users.noreply.github.com>
2024-04-21 20:06:56 -0400

    Update build_file.sh

--- build_file.sh
+++ build_file.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Ensure pip is available
-python -m ensurepip
-python -m pip install --upgrade pip
+python3 -m ensurepip
+python3 -m pip install --upgrade pip
 
 # Install dependencies from requirements.txt
-pip install -r requirements.txt
+python3 install -r requirements.txt
 
 # Execute Django collectstatic command
 python3.9 manage.py collectstatic --noinput
