33311020ee7add32bed8541a523f273ff8d9c611
JamilVelez <60303573+JamilVelez@users.noreply.github.com>
2024-04-23 14:33:47 -0400

    Update settings.py

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -35,7 +35,7 @@ ALLOWED_HOSTS = ['*']
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'staticfiles')
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Application definition
